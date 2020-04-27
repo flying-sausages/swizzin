9009a6d80aa5abc4432e398d8ccd830f90155cb6
Flying-sausages <flying_sausages@protonmail.com>
2020-04-27 23:00:35 +0000

    Added reload function in the meantime, changed user echo at the end

--- scripts/install/organizr.sh
+++ scripts/install/organizr.sh
@@ -31,62 +31,59 @@ if [[ $user == $pass ]]; then
   echo "Your username and password seem to be identical, please finish the Organizr setup manually."
   else
   echo "Setting up the organizr database"
-curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_path' \
---header 'content-type: application/x-www-form-urlencoded' \
---header 'charset: UTF-8' \
---header 'Content-Encoding: gzip' \
---header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'data[path]=/srv/organizr_db' \
---data-urlencode 'data[formKey]=' \
--sk --user "$user":"$pass" \
-| python -m json.tool >> $log 2>&1
-sleep 2
+  curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_path' \
+  --header 'content-type: application/x-www-form-urlencoded' \
+  --header 'charset: UTF-8' \
+  --header 'Content-Encoding: gzip' \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data-urlencode 'data[path]=/srv/organizr_db' \
+  --data-urlencode 'data[formKey]=' \
+  -sk --user "$user":"$pass" \
+  | python -m json.tool >> $log 2>&1
+  sleep 2
 
-    # pass="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+      # pass="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
 
-api_key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
-hash_key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
-reg_pass="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+  api_key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+  hash_key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+  reg_pass="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
 
-cat > /root/.organizr << EOF
+  cat > /root/.organizr << EOF
 API key = $api_key
 Hash key = $hash_key
 Registration pass = $reg_pass
 EOF
 
-curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_config' \
---header 'content-type: application/x-www-form-urlencoded' \
---header 'charset: UTF-8' \
---header 'Content-Encoding: gzip' \
---header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode "data[0][name]=license" \
---data-urlencode "data[0][value]=personal" \
---data-urlencode "data[1][name]=username" \
---data-urlencode "data[1][value]=${user}" \
---data-urlencode "data[2][name]=email" \
---data-urlencode "data[2][value]=root@localhost" \
---data-urlencode "data[3][name]=password" \
---data-urlencode "data[3][value]=${pass}" \
---data-urlencode "data[4][name]=hashKey" \
---data-urlencode "data[4][value]=${hash_key}" \
---data-urlencode "data[5][name]=registrationPassword" \
---data-urlencode "data[5][value]=${reg_pass}" \
---data-urlencode "data[6][name]=api" \
---data-urlencode "data[6][value]=${api_key}" \
---data-urlencode "data[7][name]=dbName" \
---data-urlencode "data[7][value]=db" \
---data-urlencode "data[8][name]=location" \
---data-urlencode "data[8][value]=/srv/organizr_db" \
--sk --user "$user":"$pass" \
-| python -m json.tool \
->> $log 2>&1
-  echo "Please restart your PHP service manually"
+  curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_config' \
+  --header 'content-type: application/x-www-form-urlencoded' \
+  --header 'charset: UTF-8' \
+  --header 'Content-Encoding: gzip' \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data-urlencode "data[0][name]=license" \
+  --data-urlencode "data[0][value]=personal" \
+  --data-urlencode "data[1][name]=username" \
+  --data-urlencode "data[1][value]=${user}" \
+  --data-urlencode "data[2][name]=email" \
+  --data-urlencode "data[2][value]=root@localhost" \
+  --data-urlencode "data[3][name]=password" \
+  --data-urlencode "data[3][value]=${pass}" \
+  --data-urlencode "data[4][name]=hashKey" \
+  --data-urlencode "data[4][value]=${hash_key}" \
+  --data-urlencode "data[5][name]=registrationPassword" \
+  --data-urlencode "data[5][value]=${reg_pass}" \
+  --data-urlencode "data[6][name]=api" \
+  --data-urlencode "data[6][value]=${api_key}" \
+  --data-urlencode "data[7][name]=dbName" \
+  --data-urlencode "data[7][value]=db" \
+  --data-urlencode "data[8][name]=location" \
+  --data-urlencode "data[8][value]=/srv/organizr_db" \
+  -sk --user "$user":"$pass" \
+  | python -m json.tool \
+  >> $log 2>&1
+    echo "Please reload your PHP service manually, or wait until your OPcache empties"
+  # . /etc/swizzin/sources/functions/php 
+  # reload_php_fpm
 
-# sleep 15
-
-# . /etc/swizzin/sources/functions/php 
-# restart_php_fpm
-# systemctl restart php7.2-fpm
 
 fi
 

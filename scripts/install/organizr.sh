03aeadc2f537d95d3f51e0d96a95523a8a2d425c
Flying-sausages <flying_sausages@protonmail.com>
2020-04-27 05:11:57 +0000

    Generating, storing and using variables

--- scripts/install/organizr.sh
+++ scripts/install/organizr.sh
@@ -35,10 +35,21 @@ curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_path' 
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'data[path]=/srv/organizr_db' \
 --data-urlencode 'data[formKey]=' \
- -sk --user "$user":"$pass" \
+-sk --user "$user":"$pass" \
 | python -m json.tool >> $log 2>&1
 sleep 2
 
+    # pass="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+
+api_key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+hash_key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+reg_pass="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c16)"
+
+cat > /root/.organizr << EOF
+API key = $api_key
+Hash key = $hash_key
+Registration pass = $reg_pass
+EOF
 #These are hardcoded for testing
 #TODO check username and password are not the same
 curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_config' \
@@ -46,27 +57,29 @@ curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_config
 --header 'charset: UTF-8' \
 --header 'Content-Encoding: gzip' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'data[0][name]=license' \
---data-urlencode 'data[0][value]=personal' \
---data-urlencode 'data[1][name]=username' \
+--data-urlencode "data[0][name]=license" \
+--data-urlencode "data[0][value]=personal" \
+--data-urlencode "data[1][name]=username" \
 --data-urlencode "data[1][value]=${user}" \
---data-urlencode 'data[2][name]=email' \
---data-urlencode 'data[2][value]=ja@nei.com' \
---data-urlencode 'data[3][name]=password' \
+--data-urlencode "data[2][name]=email" \
+--data-urlencode "data[2][value]=root@localhost" \
+--data-urlencode "data[3][name]=password" \
 --data-urlencode "data[3][value]=${pass}" \
---data-urlencode 'data[4][name]=hashKey' \
---data-urlencode 'data[4][value]=hash' \
---data-urlencode 'data[5][name]=registrationPassword' \
---data-urlencode 'data[5][value]=reg' \
---data-urlencode 'data[6][name]=api' \
---data-urlencode 'data[6][value]=ewmv12hdpsjn3cydi1r0' \
---data-urlencode 'data[7][name]=dbName' \
---data-urlencode 'data[7][value]=db' \
---data-urlencode 'data[8][name]=location' \
---data-urlencode 'data[8][value]=/srv/organizr_db' \
+--data-urlencode "data[4][name]=hashKey" \
+--data-urlencode "data[4][value]=${hash_key}" \
+--data-urlencode "data[5][name]=registrationPassword" \
+--data-urlencode "data[5][value]=${reg_pass}" \
+--data-urlencode "data[6][name]=api" \
+--data-urlencode "data[6][value]=${api_key}" \
+--data-urlencode "data[7][name]=dbName" \
+--data-urlencode "data[7][value]=db" \
+--data-urlencode "data[8][name]=location" \
+--data-urlencode "data[8][value]=/srv/organizr_db" \
 -sk --user "$user":"$pass" \
 | python -m json.tool \
 >> $log 2>&1
+echo "Please allow up to 10 minutes for the organizr database to finish setting up in the background"
+echo "You can attempt to log in with no problem, however you will not be shown the dashboard until the process is done"
 
 fi
  

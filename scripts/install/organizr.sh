b4a924fb52858380f7249ea5c8c29f590902b76c
Flying-sausages <flying_sausages@protonmail.com>
2020-04-27 04:58:27 +0000

    Working config!

--- scripts/install/organizr.sh
+++ scripts/install/organizr.sh
@@ -19,39 +19,58 @@ systemctl force-reload nginx
 mkdir /srv/organizr_db -p
 chown -R www-data:www-data /srv/organizr_db  
 
-echo "Setting up the organizr database"
-curl -X POST \
---data-urlencode "path=/srv/organizr_db" \
---data-urlencode "formKey=" \
--H "Content-Type: application/x-www-form-urlencoded" \
---user test:test \
--k -s \
-https://localhost/organizr/api/?v1/wizard_path \
+user=$(cut -d: -f1 < /root/.master.info)
+pass=$(cut -d: -f2 < /root/.master.info)
+
+
+#TODO check that passwords with weird characters will send right
+if [[ $user == $pass ]]; then 
+  echo "Your username and password seem to be identical, please finish the Organizr setup manually."
+  else
+  echo "Setting up the organizr database"
+curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_path' \
+--header 'content-type: application/x-www-form-urlencoded' \
+--header 'charset: UTF-8' \
+--header 'Content-Encoding: gzip' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'data[path]=/srv/organizr_db' \
+--data-urlencode 'data[formKey]=' \
+ -sk --user "$user":"$pass" \
 | python -m json.tool >> $log 2>&1
-
 sleep 2
-
-config_data="{ \
-    'data[0][name]': 'license', 'data[0][value]': 'personal', \
-    'data[1][name]': 'username', 'data[1][value]': 'test', \
-    'data[2][name]': 'email', 'data[2][value]': 'root@localhost', \
-    'data[3][name]': 'password', 'data[3][value]': 'testtest', \
-    'data[4][name]': 'hashKey', 'data[4][value]': 'hash', \
-    'data[5][name]': 'registrationPassword', 'data[5][value]': 'reg', \
-    'data[6][name]': 'api', 'data[6][value]': 'ewmv12hdpsjn3cydi1r0', \
-    'data[7][name]': 'dbName', 'data[7][value]': 'db', \
-    'data[8][name]': 'location', 'data[8][value]': '/srv/organizr_db' \
-}"
 
 #These are hardcoded for testing
 #TODO check username and password are not the same
-curl -X POST \
--H "Content-Type: application/x-www-form-urlencoded" \
---data "$config_data" \
---user test:test \
--k -s \
-https://localhost/organizr/api/?v1/wizard_config \
+curl --location --request POST 'https://localhost/organizr/api/?v1/wizard_config' \
+--header 'content-type: application/x-www-form-urlencoded' \
+--header 'charset: UTF-8' \
+--header 'Content-Encoding: gzip' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'data[0][name]=license' \
+--data-urlencode 'data[0][value]=personal' \
+--data-urlencode 'data[1][name]=username' \
+--data-urlencode "data[1][value]=${user}" \
+--data-urlencode 'data[2][name]=email' \
+--data-urlencode 'data[2][value]=ja@nei.com' \
+--data-urlencode 'data[3][name]=password' \
+--data-urlencode "data[3][value]=${pass}" \
+--data-urlencode 'data[4][name]=hashKey' \
+--data-urlencode 'data[4][value]=hash' \
+--data-urlencode 'data[5][name]=registrationPassword' \
+--data-urlencode 'data[5][value]=reg' \
+--data-urlencode 'data[6][name]=api' \
+--data-urlencode 'data[6][value]=ewmv12hdpsjn3cydi1r0' \
+--data-urlencode 'data[7][name]=dbName' \
+--data-urlencode 'data[7][value]=db' \
+--data-urlencode 'data[8][name]=location' \
+--data-urlencode 'data[8][value]=/srv/organizr_db' \
+-sk --user "$user":"$pass" \
 | python -m json.tool \
 >> $log 2>&1
+
+fi
+ 
+
+
 
 touch /install/.organizr.lock

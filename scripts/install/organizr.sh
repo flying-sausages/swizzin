550912b2bec399dedc6f40b38548f3861867b41b
Flying-sausages <flying_sausages@protonmail.com>
2020-04-27 03:19:54 +0000

    Changed data format to JSON

--- scripts/install/organizr.sh
+++ scripts/install/organizr.sh
@@ -29,22 +29,29 @@ curl -X POST \
 https://localhost/organizr/api/?v1/wizard_path \
 | python -m json.tool >> $log 2>&1
 
+sleep 2
+
+config_data="{ \
+    'data[0][name]': 'license', 'data[0][value]': 'personal', \
+    'data[1][name]': 'username', 'data[1][value]': 'test', \
+    'data[2][name]': 'email', 'data[2][value]': 'root@localhost', \
+    'data[3][name]': 'password', 'data[3][value]': 'testtest', \
+    'data[4][name]': 'hashKey', 'data[4][value]': 'hash', \
+    'data[5][name]': 'registrationPassword', 'data[5][value]': 'reg', \
+    'data[6][name]': 'api', 'data[6][value]': 'ewmv12hdpsjn3cydi1r0', \
+    'data[7][name]': 'dbName', 'data[7][value]': 'db', \
+    'data[8][name]': 'location', 'data[8][value]': '/srv/organizr_db' \
+}"
+
 #These are hardcoded for testing
 #TODO check username and password are not the same
 curl -X POST \
---data-urlencode "license=personal" \
---data-urlencode "username=test" \
---data-urlencode "email=root@localhost" \
---data-urlencode "password=testtest" \
---data-urlencode "hashKey=hash" \
---data-urlencode "registrationPassword=reg" \
---data-urlencode "api=ewmv12hdpsjn3cydi1r0" \
---data-urlencode "dbName=db" \
---data-urlencode "location=/srv/organizr_db" \
 -H "Content-Type: application/x-www-form-urlencoded" \
+--data "$config_data" \
 --user test:test \
 -k -s \
 https://localhost/organizr/api/?v1/wizard_config \
-| python -m json.tool >> $log 2>&1
+| python -m json.tool \
+>> $log 2>&1
 
 touch /install/.organizr.lock
